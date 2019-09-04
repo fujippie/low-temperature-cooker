8d6d415982515456225e84a9837a15c3ab595926
semanticgaps@gmail.com <fujiwara3>
2019-09-04 20:04:49 +0900

    a

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import datetime as datetime
 #running_time :稼働時間(秒)　#check_interval :温度計測間隔(秒)　#max_temperature:設定温度(℃)
 def main(running_time,check_interval,max_temperature):
     #ログファイル
-    log = open(  "log/"+datetime.datetime.now().strftime("%Y-%m-%d_%H-%M")  +".txt",'w')
+    log = open(  "log/"+datetime.datetime.now().strftime("%m-%d_%H-%M")  +".txt",'w')
     log.write("start\n")
 
     pod = opPod.OperatePod()
@@ -15,9 +15,9 @@ def main(running_time,check_interval,max_temperature):
     start = time.time()
     elapse = 0
 
-    while elapse - start <= running_time:
+    while elapse - start >= running_time:
         temperature = temper.getTemperature()
-        log.write("時間"+ datetime.datetime.now().strftime("%H-%M-%S") +"/温度:" + str( temperature ) +"℃")
+        log.write("時間"+ datetime.datetime.now().strftime("%H-%M-%S") +"/温度:" + str( temperature ) +"℃\n")
         if temperature <= max_temperature :
             pod.turn_on()
         elif temperature >= max_temperature:
@@ -27,5 +27,5 @@ def main(running_time,check_interval,max_temperature):
     pod.end()
     log.close()
 
-main(10,5,60)
+main(30*60,5,60)
 
