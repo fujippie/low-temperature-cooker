a655f94e6bf2822af08a7b2614a735ca77fae138
semanticgaps@gmail.com <fujiwara3>
2019-09-04 19:44:21 +0900

    dd

--- log.py
+++ log.py
@@ -5,6 +5,6 @@ now = datetime.datetime.now()
 a = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M")
 
 print(now.strftime("%Y%m%d%H%M%S"))
-log = open(datetime.datetime.now().strftime("%Y-%m-%d_%H-%M")+".txt",'w')
+log = open("./log/"+datetime.datetime.now().strftime("%Y-%m-%d_%H-%M")+".txt",'w')
 log.write("aaa\n")
 log.write("asdsdsdaa\n")
--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import datetime as datetime
 #running_time :稼働時間(秒)　#check_interval :温度計測間隔(秒)　#max_temperature:設定温度(℃)
 def main(running_time,check_interval,max_temperature):
     #ログファイル
-    log = open(  datetime.datetime.now().strftime("%Y-%m-%d_%H-%M")  +".txt",'w')
+    log = open(  "log/"+datetime.datetime.now().strftime("%Y-%m-%d_%H-%M")  +".txt",'w')
     log.write("start\n")
 
     pod = opPod.OperatePod()
@@ -15,17 +15,17 @@ def main(running_time,check_interval,max_temperature):
     start = time.time()
     elapse = 0
 
-    while elapse - start != running_time:
+    while elapse - start <= running_time:
         temperature = temper.getTemperature()
         log.write("時間"+ datetime.datetime.now().strftime("%H-%M-%S") +"/温度:" + str( temperature ) +"℃")
         if temperature <= max_temperature :
             pod.turn_on()
         elif temperature >= max_temperature:
             pod.turn_off()
-        time.sleep(check_interval) #20秒ごとに温度を測定
+        time.sleep(check_interval) 
         elapse = time.time()
     pod.end()
-
+    log.close()
 
 main(10,5,60)
 
